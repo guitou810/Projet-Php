3c053c2953788b4fd5d39cefa75a484987a6d112
Eleve <gdax95@gmail.com>
2019-10-21 15:49:43 +0200

    show

--- Views/Show.php
+++ Views/Show.php
@@ -6,20 +6,7 @@ require_once('../Controllers/index.php');
     
     <body>
         <h1>BUG INFORMATIONS</h1>
-        <?php
-        
-        echo 
-          "<div class='form-example' style='width:100%'>
-        <label for='name'>Title: </label>
-            ".$Bugss->get_title()."
-        </div>
-        
-        <div class='form-example' style='width:100%'>
-        <label for='name'>Description: </label>
-            ".$Bugss->getDescription()."
-        </div>"
 
-        ?>
         <a href="list.php"><input class="favorite styled "type="button" value="Retour" name="action"</a>
     </body>
 
